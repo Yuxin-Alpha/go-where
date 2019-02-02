95ff5df74d5c35520aa104bf2387054ae36ec75a
Clement_Alpha <956163372@qq.com>
2019-02-02 17:14:55 +0800

    首页头部布局

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="header">
+    <div class="header-left">返回</div>
+    <div class="header-input">输入城市/景点/游玩主题</div>
+    <div class="header-right">城市</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .header
+    display: flex
+    line-height: .86rem
+    background-color: #00bcd4
+    color: #fff
+    .header-left
+      width: .664rem
+      float: left
+    .header-input
+      flex: 1
+      margin-top: .12rem
+      margin-left: .2rem
+      height: .64rem
+      line-height: .64rem
+      color: #ccc
+      background-color: #fff
+      border-radius: .1rem
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+</style>
