6f60afe54b8691b08f2fe78e22a5225098bd88bd
Clement_Alpha <956163372@qq.com>
2019-02-12 15:25:06 +0800

    周末去哪组件

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -4,6 +4,7 @@
     <home-swiper></home-swiper>
     <home-icons></home-icons>
     <home-recommend></home-recommend>
+    <home-weekend></home-weekend>
   </div>
 </template>
 
@@ -12,10 +13,11 @@ import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
 import HomeRecommend from './components/Recommend'
+import HomeWeekend from './components/Weekend'
 export default {
   name: 'Home',
   components: {
-    HomeHeader, HomeSwiper, HomeIcons, HomeRecommend
+    HomeHeader, HomeSwiper, HomeIcons, HomeRecommend, HomeWeekend
   }
 }
 </script>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <div class="recommend-title">周末去哪儿</div>
+    <ul>
+      <li
+        class="item border-bottom"
+        v-for="item of recommendList"
+        :key="item.id">
+        <div class="item-img-wrapper">
+          <img class="item-img" :src="item.imgUrl">
+        </div>
+        <div class="item-info">
+          <p class="item-title">{{ item.title }}</p>
+          <p class="item-desc">{{ item.desc }}</p>
+        </div>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/f3/86173f863bef61.jpg_r_640x214_52b003ac.jpg',
+        title: '京城周末撒欢',
+        desc: '北京首都'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1510/6e/1ea71e2f04e.jpg_r_640x214_aa6f091d.jpg',
+        title: '京城有好泉水',
+        desc: '细数北京温泉，温暖你的冬天'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1811/7e/476589267ebb41.jpg_r_640x214_bf599709.jpg',
+        title: '帝都动物园',
+        desc: '1321321321321'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .recommend-title
+    margin-top: .2rem
+    line-height: .8rem
+    background-color: #eee
+    text-indent: .2rem
+  .item-img-wrapper
+    height: 0
+    overflow: hidden
+    padding-bottom: 33.9%
+  .item-img
+    width: 100%
+  .item-info
+    padding: .1rem
+    .item-title
+      line-height: .54rem
+      font-size: .32rem
+    .item-desc
+      line-height: .4rem
+      color: #ccc
+</style>
