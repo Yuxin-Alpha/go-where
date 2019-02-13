6e9ab432f5eb9f0a423f6f86c3d8e34073e2811c
Clement_Alpha <956163372@qq.com>
2019-02-13 10:43:01 +0800

    城市搜索组件

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search">
+    <input class="search-input" type="text" placeholder="输入城市名或拼音">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~style/varibles.styl"
+  .search
+    padding: 0 .1rem
+    height: .72rem
+    background: $bgColor
+    .search-input
+      box-sizing: border-box
+      width: 100%
+      height: .62rem
+      padding: 0 .1rem
+      line-height: .62rem
+      text-align: center
+      border-radius: .06rem
+      color: #666
+</style>
