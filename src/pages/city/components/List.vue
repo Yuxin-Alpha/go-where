d8dd7c1e217d45b0b963c98971a776ae2cadfe49
Clement_Alpha <956163372@qq.com>
2019-02-13 13:32:07 +0800

    字母表样式

---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,123 @@
+<template>
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">
+          当前城市
+        </div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">
+              北京
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">
+          热门城市
+        </div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">
+              北京
+            </div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">
+              北京
+            </div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">
+              北京
+            </div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">
+              北京
+            </div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">
+              北京
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">
+          A
+        </div>
+        <div class="item-list">
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Bscroll from 'better-scroll'
+export default {
+  name: 'CityList',
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.wrapper)
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import "~style/varibles.styl"
+  .border-topbottom
+    &:before
+      border-color: #ccc
+    &:after
+      border-color: #ccc
+  .border-bottom
+  &:before
+    border-color: #ccc
+  .list
+    overflow: hidden
+    position: absolute
+    top: 1.58rem
+    left: 0
+    right: 0
+    bottom: 0
+    .title
+      line-height: .54rem
+      background: #eee
+      padding-left: .2rem
+      font-size: .26rem
+      color: #666
+    .button-list
+      overflow: hidden
+      padding: .1rem .6rem .1rem .1rem
+      .button-wrapper
+        width: 33.33%
+        float: left
+        .button
+          padding: .1rem
+          margin: .1rem 0
+          text-align: center
+          border: .02rem solid #ccc
+          border-radius: .06rem
+    .item-list
+      .item
+        line-height: .76rem
+        padding-left: .2rem
+</style>
