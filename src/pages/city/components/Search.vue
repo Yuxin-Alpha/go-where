167046d42502e5a0c980989681fbb5cd5bf92f41
Clement_Alpha <956163372@qq.com>
2019-02-15 10:44:39 +0800

    详情页面头部

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -75,7 +75,6 @@ export default {
       this.$router.push('/')
     }
   }
-
 }
 </script>
 
---
+++ src/pages/detail/components/Banner.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="banner">
+    <img class="banner-img" src="//img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_600x330_f922b488.jpg" alt="">
+    <div class="banner-info">
+      <div class="banner-title">故宫AAAA景区</div>
+      <div class="banner-number">
+        <span class="iconfont banner-icon">&#xe64a;</span>
+        39
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailBanner'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .banner
+    position relative
+    overflow hidden
+    height 0
+    padding-bottom 55%
+    .banner-img
+      width 100%
+    .banner-info
+      display flex
+      position absolute
+      left 0
+      right 0
+      bottom 0
+      line-height .6rem
+      color #fff
+      background-image linear-gradient(top, rgba(0, 0, 0, 0), rgba(55, 0, 0, 0.8))
+      .banner-title
+        flex 1
+        font-size .32rem
+        padding 0 .2rem
+      .banner-number
+        line-height .32rem
+        height .32rem
+        margin-top .14rem
+        padding 0 .4rem
+        border-radius .2rem
+        background rgba(0, 0, 0, .8)
+        font-size .24rem
+        .banner-icon
+          font-size .24rem
+</style>
